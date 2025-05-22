a438cc3aa4e24582162bd96f72c6347cb489baa4
HectorS89 <hectorsiri89@gmail.com>
2025-05-21 21:58:24 -0300

    mi primer componente

---
+++ components/banner.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <>
+    <h1 className='bg-indigo-600'>banner</h1>
+    <p>Mi primer componente</p>
+    </>
+
+  )
+}
+
+export default Banner
+
+
+
